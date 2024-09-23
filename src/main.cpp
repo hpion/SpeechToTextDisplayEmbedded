396f80d7cd46e98dde579b660233ccb49a760934
hpion <hunter.pion@gmail.com>
2024-09-23 09:41:46 -0400

    Moved wrap method to a seperate cpp file. Rewrote wrap method. Implemented a loop to call wrap() in main. Added test function to display output in terminal as well as on the display.

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,7 @@
 #include <string.h>
 #include <U8g2lib.h>
 #include <SPI.h>
+#include <wrapping.cpp>
 
 //definitions
 #define OLED_DC  D2
@@ -35,68 +36,53 @@ void display(char *str);
 
 void setup() {
   u8g2.begin();           //start u8g2
+  //TEST
+  Serial.begin(115200);  //open serial connection for debugging
+  //TEST
 }
 
 void loop() {
-  char testStr[] = "this is a test string to handle wrapping"; //create test string
-  display(testStr);
-  delay(2000);
+  char input[] = "this is a test string to handle wrapping"; //create test string
+  int result = 0; //initialize result as 0
+  do
+  {
+    char disp[3][32] = {'/0'};  //initialize array of three C strings
+    result = wrap(disp, input, result);
+    //if result is -1 display an error message and break early
+    if (result == -1)
+    {
+      //write error message to disp[0]
+      char failMessage[32] = "Failed to display text.";
+      for (int i = 0; i < 32; i++)
+      {
+        disp[0][i] = failMessage[i];
+      }
+      //clear disp 1 and 2
+      disp[1][0] = '/0';
+      disp[2][0] = '/0';
+      //display disp
+      display(disp);
+      //wait 10s
+      delay(10000);
+      //break
+      break;
+    }
+    //otherwise the method was successfully completed, display the output and wait 5s
+    display(disp);
+    delay(5000);
+  }
+  while (result != 0);
 }
 
 //function to display text
-//TODO - remove whitespace on first characters of lines
-//TODO - wrapping based on words rather than characters
-void display(char *str)
+void display(char disp[3][32])
 {
-  //handle wrapping by splitting the string into up to 3 lines of 31 chars
-  char str1[32], str2[32], str3[32] = {'\0'};  //create three 32 byte string arrays
-  int nextSpace = 0;  //used to find the next space in str
-  int currStr = 1;    //current string to write into
-
-  for (int i = 0; i < strlen(str); i++)
-  {
-    //read next character into current string
-    if (currStr == 1)
-    {
-      str1[i] = str[i];
-    }
-
-    else if (currStr == 2)
-    {
-      str2[i % 31] = str[i];
-    }
-
-    else if (currStr == 3)
-    {
-      str3[i % 31] = str[i];
-    }
-    //if currStr is 4 store the current i value in nextSpace and break
-    else if (currStr == 4)
-    {
-      nextSpace = i;
-      break;
-    }
-
-    //if i has caught up to nextSpace, find the next space
-    if (i == nextSpace)
-    {
-      for (int j = i; j < strlen(str); j++)
-      {
-        //update nextSpace
-        if (str[j] == ' ' || str[j] == '\0')
-        {
-          nextSpace = j;
-          //if nextSpace is after the end of the current string, move to the next string
-          if ((i % 31) + 1 + nextSpace - i >= 31)
-          {
-            currStr++;
-          }
-          break;
-        }
-      }
-    }
-  }
-
+  //TEST
+  //display the output to the terminal
+  Serial.println(disp[0]);
+  Serial.println(disp[1]);
+  Serial.println(disp[2]);
+  //TEST
   //display text
   u8g2.firstPage();
   do
@@ -104,9 +90,9 @@ void display(char *str)
     u8g2.clearBuffer();                      //clear memory
     u8g2.setFont(u8g2_font_4x6_mf);          //set font
     u8g2.setFontPosTop();                    //start writing in the top left corner of the display
-    u8g2.drawStr(0, 5, str1);                //write str1 to memory
-    u8g2.drawStr(0, 15, str2);               //write str2 to memory
-    u8g2.drawStr(0, 25, str3);               //write str3 to memory
+    u8g2.drawStr(0, 5, disp[0]);             //write disp[0] to memory
+    u8g2.drawStr(0, 15, disp[1]);            //write disp[1] to memory
+    u8g2.drawStr(0, 25, disp[2]);            //write disp[2] to memory
     u8g2.sendBuffer();                       //display memory content
   } while(u8g2.nextPage());
 }
---
+++ src/wrapping.cpp
@@ -0,0 +1,69 @@
+/*
+Author: Hunter Pion
+Created: 9/23/2024
+Edited:  9/23/2024
+
+This program contains methods to determine wrapping for a string based on the words
+contained in it, delimited by spaces.
+
+The wrap(char**, char*) method will fill the provided array of three C strings of length 32
+using the provided C string.
+
+The three strings are to be displayed on each of the three lines on the display.
+
+The method will return an int value,
+If the return value is 0, the method is comlete.
+If the return value is -1, the method failed.
+Any other value corresponds to the index to start the next three strings of text on if the passed string
+cannot be displayed using the three lines available.
+*/
+
+//include statements
+#include <string.h>
+
+//wrap string method
+int wrap(char output[3][32], char input[], int start)
+{
+    int currStr = 0;    //store current string to write to
+    int pointer = 0;    //write location in current string
+    //for loop to move through input
+    for (int i = start; i < strlen(input) && currStr < 4; i++)
+    {
+        //if currStr is 4, return i
+        if (currStr == 4)
+        {
+            return i;
+        }
+        //write the current character in input into the current string
+        output[currStr][pointer] = input[i];
+        //if this is the end of the string, determine wrap location
+        if (pointer == 31)
+        {
+            //if i is 0, the method failed because the first word was too long, return -1
+            if (i == 0)
+            {
+                return -1;
+            }
+            //if this character is a space or the end of the input string, no extra action is required
+            else if (output[currStr][pointer] == ' ' || output[currStr][pointer] == '/0'){}
+            //if the next character is a space or the end of the input string, increment i an extra time to skip it
+            else if (input[i + 1] == ' ' || input[i + 1] == '/0')
+            {
+                i++;
+            }
+            //if the prior conditions were false, this is the middle of a word, move i back to the last space
+            else
+            {
+                while(input[i] != ' ')
+                {
+                    i--;
+                }
+            }
+            //set pointer to 0 and increment currStr
+            pointer = 0;
+            currStr++;
+        }
+    }
+    //if this point is reached, the method was successful and the full input string was used, return 0
+    return 0;
+}
